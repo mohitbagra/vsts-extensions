b9c2d662d3bb4b8c3707b40d309aee2074338b5b
Mohit Bagra <mbagra@microsoft.com>
2018-04-08 10:52:11 -0400

    fix team get client

--- src/Library/Flux/Actions/TeamActions.ts
+++ src/Library/Flux/Actions/TeamActions.ts
@@ -4,6 +4,7 @@ import { TeamStore } from "Library/Flux/Stores/TeamStore";
 import { localeIgnoreCaseComparer } from "Library/Utilities/String";
 import { WebApiTeam } from "TFS/Core/Contracts";
 import * as CoreClient from "TFS/Core/RestClient";
+import * as VSS_Service from "VSS/Service";
 
 export namespace TeamActions {
     const teamStore: TeamStore = StoreFactory.getInstance<TeamStore>(TeamStore);
@@ -30,11 +31,11 @@ export namespace TeamActions {
     async function getTeams(): Promise<WebApiTeam[]> {
         const teams: WebApiTeam[] = [];
         const top: number = 300;
-        const client = CoreClient.getClient();
+        const client = await VSS_Service.getClient<CoreClient.CoreHttpClient4>(CoreClient.CoreHttpClient4);
         const project = VSS.getWebContext().project.id;
 
         const getTeamDelegate = async (skip: number) => {
-            const result: WebApiTeam[] = await client.getTeams(project, false, top, skip);
+            const result: WebApiTeam[] = await client.getTeams(project, top, skip);
             if (result.length > 0) {
                 teams.push(...result);
             }
