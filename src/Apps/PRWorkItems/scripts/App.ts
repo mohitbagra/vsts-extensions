e2d5679550afeefe0553d4084072bef349e2318a
Mohit Bagra <mohitbagra@gmail.com>
2018-05-23 22:10:20 -0700

    PR workitems

--- src/Apps/PRWorkItems/scripts/App.ts
+++ src/Apps/PRWorkItems/scripts/App.ts
@@ -1,39 +1,80 @@
+import { contains } from "Common/Utilities/Array";
+import * as ExtensionDataManager from "Common/Utilities/ExtensionDataManager";
 import { reloadPage } from "Common/Utilities/Navigation";
+import { stringEquals } from "Common/Utilities/String";
 import { getFormNavigationService } from "Common/Utilities/WorkItemFormHelpers";
+import { WorkItemType } from "TFS/WorkItemTracking/Contracts";
 import * as WitClient from "TFS/WorkItemTracking/RestClient";
 import { JsonPatchDocument, JsonPatchOperation, Operation } from "VSS/WebApi/Contracts";
 
 const extensionContext = VSS.getExtensionContext();
+let configuredWorkItemTypes: WorkItemType[];
 
 VSS.register(`${extensionContext.publisherId}.${extensionContext.extensionId}.context-menu`, () => ({
-    execute: async (actionContext) => {
+    getMenuItems: async (actionContext) => {
         if (actionContext && actionContext.pullRequest && actionContext.pullRequest.artifactId) {
-            const workItemNavSvc = await getFormNavigationService();
-            const workItem = await workItemNavSvc.openNewWorkItem("Bug");
+            const [workItemNavSvc, witClient] = await Promise.all([
+                getFormNavigationService(),
+                WitClient.getClient()
+            ]);
+            if (!configuredWorkItemTypes) {
+                const projectId = VSS.getWebContext().project.id;
+                const [configuredWorkItemTypesSetting, allWorkItemTypes] = await Promise.all([
+                    ExtensionDataManager.readSetting(`wits_${projectId}`, ["Bug", "User Story"], false),
+                    witClient.getWorkItemTypes(projectId)
+                ]);
 
-            if (workItem && workItem.id) {
-                const witClient = WitClient.getClient();
-                const patchDocument: JsonPatchDocument & JsonPatchOperation[] = [
-                    {
-                        op: Operation.Add,
-                        path: "/relations/-",
-                        value: {
-                            rel: "ArtifactLink",
-                            url: actionContext.pullRequest.artifactId,
-                            attributes: {
-                                name: "Pull Request"
-                            }
-                        }
-                    } as JsonPatchOperation
-                ];
-                try {
-                    await witClient.updateWorkItem(patchDocument, workItem.id);
-                    reloadPage();
-                }
-                catch (e) {
-                    console.warn(`Cannot create workitem: ${e.message}`);
-                }
+                configuredWorkItemTypes = allWorkItemTypes.filter(w => contains(configuredWorkItemTypesSetting, w.name, (w1, w2) => stringEquals(w1, w2, true)));
             }
+
+            const childMenuItems: IContributedMenuItem[] = configuredWorkItemTypes.map(w => ({
+                text: w.name,
+                title: w.name,
+                action: async () => {
+                    const workItem = await workItemNavSvc.openNewWorkItem(w.name);
+                    if (workItem && workItem.id) {
+                        const patchDocument: JsonPatchDocument & JsonPatchOperation[] = [
+                            {
+                                op: Operation.Add,
+                                path: "/relations/-",
+                                value: {
+                                    rel: "ArtifactLink",
+                                    url: actionContext.pullRequest.artifactId,
+                                    attributes: {
+                                        name: "Pull Request"
+                                    }
+                                }
+                            } as JsonPatchOperation
+                        ];
+                        try {
+                            await witClient.updateWorkItem(patchDocument, workItem.id);
+                            reloadPage();
+                        }
+                        catch (e) {
+                            console.warn(`Cannot create workitem: ${e.message}`);
+                        }
+                    }
+                }
+            }));
+            childMenuItems.push(
+                {
+                    separator: true
+                },
+                {
+                    text: "Configure",
+                    title: "Configure Work item types",
+                    action: async () => {
+                        // open dialog
+                    }
+                }
+            );
+
+            return [{
+                text: "Link to a new workitem",
+                title: "Create a new workitem and link to this Pull request",
+                icon: `${VSS.getExtensionContext().baseUri}/images/logo.png`,
+                childItems: childMenuItems
+            }];
         }
     }
 }));
